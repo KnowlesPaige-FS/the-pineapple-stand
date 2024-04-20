567114a76825dfc0edd75d8c579e30fa46965c03
Paige Knowles <paknowles@student.fullsail.edu>
2024-04-20 10:00:36 -0400

    update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <section>
           <Navigation/>
         </section>
-
+        
         <section>
           <Routes>
             <Route path='/' element={<Home />} />
