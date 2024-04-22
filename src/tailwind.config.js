9c45926f35bcf07ef0c23e351cbd85d40344d32e
Paige Knowles <paknowles@student.fullsail.edu>
2024-04-22 15:34:41 -0400

    update styles

---
+++ src/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          customBackground: '#B9B1A3',
+          customHeadings: '#8B8E51',
+          customTexts: '#5A543D',
+          customButtons: '#E1A758',
+        },
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+          serif: ['Arapey', 'serif']
+        },
+      },
+    },
+    plugins: [],
+  }
